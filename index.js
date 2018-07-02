3a9444decc278ecbd48c127bde4daa6a969e50d3
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-07-02 19:10:16 +0000

    code

--- index.js
+++ index.js
@@ -2,123 +2,33 @@ var Benchmark = require('benchmark');
 var tb = require('travis-benchmark');
 var async = require('async');
 
-var is = require('is');
 var lodash = require('lodash');
-var underscore = require('underscore');
-var isString = require('is-string');
-var isNumber = require('is-number');
-var isArray = require('is-array');
-var isObject = require('is-object');
-var isFunction = require('is-function');
-var isUndefined = require('is-undefined');
-var isNull = require('is-null');
-var isBoolean = require('is-boolean');
 
-var demoString = 'abc';
-var demoNumber = 123;
-var demoObject = {};
-var demoArray = [];
-var demoFunction = function() {};
-var demoUndefined = undefined;
-var demoNull = null;
-var demoBoolean = false;
+var uuidv1 = require('uuid/v1');
+var uuidv3 = require('uuid/v3');
+var uuidv4 = require('uuid/v4');
+var uuidv5 = require('uuid/v5');
 
-async.series([
-  function(next) {
-    var suite = new Benchmark.Suite(`string`);
-    
-    suite.add('typeof [string]', function() { typeof demoString === 'string'; });
-    suite.add('is@3.2.1 string', function() { is.string(demoString); });
-    suite.add('lodash@4.17.10 isString', function() { lodash.isString(demoString); });
-    suite.add('underscore@1.9.1 isString', function() { underscore.isString(demoString); });
-    suite.add('isString@1.0.4', function() { isString(demoString); });
-    
-    tb.wrapSuite(suite, function() { next(); });
-    suite.run({ async: true });
-  },
-  function(next) {
-    var suite = new Benchmark.Suite(`number`);
-    
-    suite.add('typeof [number]', function() { typeof demoNumber === 'number'; });
-    suite.add('is@3.2.1 number', function() { is.number(demoNumber); });
-    suite.add('lodash@4.17.10 isNumber', function() { lodash.isNumber(demoNumber); });
-    suite.add('underscore@1.9.1 isNumber', function() { underscore.isNumber(demoNumber); });
-    suite.add('isNumber@6.0.0', function() { isNumber(demoNumber); });
-    
-    tb.wrapSuite(suite, function() { next(); });
-    suite.run({ async: true });
-  },
-  function(next) {
-    var suite = new Benchmark.Suite(`array`);
-    
-    suite.add('Array.isArray', function() { Array.isArray(demoArray); });
-    suite.add('[object Array]', function() { Object.prototype.toString.call(demoArray) === '[object Array]' });
-    suite.add('is@3.2.1 array', function() { is.array(demoArray); });
-    suite.add('lodash@4.17.10 isArray', function() { lodash.isArray(demoArray); });
-    suite.add('underscore@1.9.1 isArray', function() { underscore.isArray(demoArray); });
-    suite.add('isArray@1.0.1', function() { isArray(demoArray); });
-    
-    tb.wrapSuite(suite, function() { next(); });
-    suite.run({ async: true });
-  },
-  function(next) {
-    var suite = new Benchmark.Suite(`object`);
-    
-    suite.add('typeof [object]', function() { typeof demoObject === 'object'; });
-    suite.add('is@3.2.1 object', function() { is.object(demoObject); });
-    suite.add('lodash@4.17.10 isObject', function() { lodash.isObject(demoObject); });
-    suite.add('underscore@1.9.1 isObject', function() { underscore.isObject(demoObject); });
-    suite.add('isObject@1.0.1', function() { isObject(demoObject); });
-    
-    tb.wrapSuite(suite, function() { next(); });
-    suite.run({ async: true });
-  },
-  function(next) {
-    var suite = new Benchmark.Suite(`function`);
-    
-    suite.add('typeof [function]', function() { typeof demoFunction === 'function'; });
-    suite.add('is@3.2.1 function', function() { is.function(demoFunction); });
-    suite.add('lodash@4.17.10 isFunction', function() { lodash.isFunction(demoFunction); });
-    suite.add('underscore@1.9.1 isFunction', function() { underscore.isFunction(demoFunction); });
-    suite.add('isFunction@1.0.1', function() { isFunction(demoFunction); });
-    
-    tb.wrapSuite(suite, function() { next(); });
-    suite.run({ async: true });
-  },
-  function(next) {
-    var suite = new Benchmark.Suite(`undefined`);
-    
-    suite.add('typeof [undefined]', function() { typeof demoUndefined === 'undefined'; });
-    suite.add('is@3.2.1 undefined', function() { is.undefined(demoUndefined); });
-    suite.add('lodash@4.17.10 isUndefined', function() { lodash.isUndefined(demoUndefined); });
-    suite.add('underscore@1.9.1 isUndefined', function() { underscore.isUndefined(demoUndefined); });
-    suite.add('isUndefined@1.0.9', function() { isUndefined(demoUndefined); });
-    
-    tb.wrapSuite(suite, function() { next(); });
-    suite.run({ async: true });
-  },
-  function(next) {
-    var suite = new Benchmark.Suite(`null`);
-    
-    suite.add('typeof [null]', function() { typeof demoNull === 'null'; });
-    suite.add('is@3.2.1 null', function() { is.null(demoNull); });
-    suite.add('lodash@4.17.10 isNull', function() { lodash.isNull(demoNull); });
-    suite.add('underscore@1.9.1 isNull', function() { underscore.isNull(demoNull); });
-    suite.add('isNull', function() { isNull(demoNull); });
-    
-    tb.wrapSuite(suite, function() { next(); });
-    suite.run({ async: true });
-  },
-  function(next) {
-    var suite = new Benchmark.Suite(`boolean`);
-    
-    suite.add('typeof [boolean]', function() { typeof demoBoolean === 'boolean'; });
-    suite.add('is@3.2.1 boolean', function() { is.boolean(demoBoolean); });
-    suite.add('lodash@4.17.10 isBoolean', function() { lodash.isBoolean(demoBoolean); });
-    suite.add('underscore@1.9.1 isBoolean', function() { underscore.isBoolean(demoBoolean); });
-    suite.add('isBoolean', function() { isBoolean(demoBoolean); });
-    
-    tb.wrapSuite(suite, function() { next(); });
-    suite.run({ async: true });
-  },
-]);
+var chance = require('chance')();
+var randomId = require("random-id");
+var shortid = require('shortid');
+var uniqid = require('uniqid');
+
+var suite = new Benchmark.Suite(`speed unique id`);
+
+suite.add('uuid@3.3.2 uuidv1', function() { uuidv1(); });
+suite.add('uuid@3.3.2 uuidv3', function() { uuidv3('hello.example.com', uuidv3.DNS); });
+suite.add('uuid@3.3.2 uuidv4', function() { uuidv4(); });
+suite.add('uuid@3.3.2 uuidv5', function() { uuidv5('hello.example.com', uuidv5.DNS); });
+suite.add('chance@1.0.16 android_id', function() { chance.android_id(); });
+suite.add('chance@1.0.16 apple_token', function() { chance.apple_token(); });
+suite.add('chance@1.0.16 bb_pin', function() { chance.bb_pin(); });
+suite.add('chance@1.0.16 wp7_anid', function() { chance.wp7_anid(); });
+suite.add('chance@1.0.16 wp8_anid2', function() { chance.wp8_anid2(); });
+suite.add('chance@1.0.16 fbid', function() { chance.fbid(); });
+suite.add('random-id@0.0.2', function() { randomId(); });
+suite.add('shortid@2.2.8', function() { shortid.generate(); });
+suite.add('uniqid@5.0.3', function() { uniqid() });
+
+tb.wrapSuite(suite, function() { next(); });
+suite.run({ async: true });
